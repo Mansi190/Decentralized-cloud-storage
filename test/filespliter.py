25547ec57d753e85be127f090c4b6164dc9b69af
tanish20j <tanish20j@gmail.com>
2021-10-05 22:00:20 +0530

    sha3_256 implementation in test

--- test/filespliter.py
+++ test/filespliter.py
@@ -1,4 +1,16 @@
 # Write our file splitting program here
- 
 
-print("hellow world")
+import sys
+import hashlib
+if sys.version_info < (3, 6):
+    import sha3
+ 
+# initiating the "s" object to use the
+# sha3_224 algorithm from the hashlib module.
+s = hashlib.sha3_256()
+
+# providing the input to the hashing algorithm.
+s.update(b"Hello World")
+
+print("Hellow World")
+print("Sha3_256 :"+ s.hexdigest())
